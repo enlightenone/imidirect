16637f92b93041c4ef88c9eac7b85d3508112540
enlightenone <stevenwchow@gmail.com>
2015-03-02 19:09:35 -0800

    pre pdf functionality configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
     resources :forms
+    resources :pdfs
     post 'forms/:id/new_case' => 'forms#new_case', as: :new_case 
     get  'forms/:id/new_application_form' => 'forms#new_application_form', as: :new_application_form
 

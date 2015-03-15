894793eff43c6ec671fee2502bc6eaaf2ade5c43
enlightenone <stevenwchow@gmail.com>
2015-03-15 11:53:40 -0700

    i130 form switch button function development is in progress

--- app/assets/javascripts/ng-app/main.js
+++ app/assets/javascripts/ng-app/main.js
@@ -23,7 +23,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
     
         // route to show our basic form (/form)section1&
         .state('form', {
-            url: '/form',
+            url: '/form?application_type&section1&section2&section3&section4&section5',
+            // url: '/form',
             templateUrl: 'forms/form.html',
             controller: 'formController'
         })
@@ -36,16 +37,17 @@ app.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: 'cases/i130/section1.html'
 
         })
-        
+
         // url will be /form/interests
         .state('form.section2', {
             url: '/section2',
+             // templateUrl: 'cases/i130/section2.html'
 
             templateProvider: function($http, $stateParams){
 
                 var obj = $stateParams;
-                // var templateName = "cases/" + obj.application_type + "/" + "section2.html" ;
-                var templateName = 'cases/i130/section1.html'; 
+                console.log("parameter type: " + obj.application_type)
+                var templateName = "cases/" + obj.application_type + "/" + "section2.html" ;
                 console.log("template Name: " + templateName);
                 
 
@@ -95,7 +97,12 @@ app.config(function($stateProvider, $urlRouterProvider) {
         .state('form.section8', {
             url: '/section8',
             templateUrl: 'cases/i130/section8.html'
+        })
+        .state('form.section9', {
+            url: '/section9',
+            templateUrl: 'cases/i130/section8.html'
         });
+
     // catch all route
     // send users to the form page 
     $urlRouterProvider.otherwise('form/section1?application_type=i130&section1&section2&section3&section4&section5');
@@ -103,6 +110,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 
 app.controller("formController", function($scope) {
+
+    //Object containing I-130 relative application options 
+    $scope.formOptions = {} ; 
     
     // we will store all of our form data in this object
     // $scope.formData = obj2.section1;
@@ -110,7 +120,24 @@ app.controller("formController", function($scope) {
 
     //function to choose forms
     $scope.chooseForm = function(category) {
-        location.assign('/apps#/form/section1');
+
+    $scope.switchButtons = {}; //Create object to arrange form/section combination.
+    var i = 3; 
+
+    // Loop through formOptions to assign form to specific switch button.
+    for (key in $scope.formOptions){
+        if($scope.formOptions[key] == true){
+            i++;
+       $scope.switchButtons["section" + i] = key;  
+    }
+    }
+    // Assign submit button to the last section.
+    i++;
+    $scope.switchButtons["section" + i] = "submit";
+    console.log($scope.switchButtons);
+    location.assign('form/section1?application_type=i130&section1=i130-applicant&section2=i130-sponsor&section3=i130-option' +
+     '&section4=' + $scope.switchButtons["section4"] +'&section5=' + $scope.switchButtons["section5"] + '&section6=' +
+     $scope.switchButtons["section6"] + '&section7=' + $scope.switchButtons["section7"] + '&section8=' + '&section9=');
     };
     
     // function to process the form

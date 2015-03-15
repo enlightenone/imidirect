b53d1e9c5e17446a25d2da1f9b9f1ae8badebf3f
enlightenone <stevenwchow@gmail.com>
2015-03-14 19:18:22 -0700

    i130 form option in progress

--- app/assets/javascripts/ng-app/main.js
+++ app/assets/javascripts/ng-app/main.js
@@ -23,14 +23,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
     
         // route to show our basic form (/form)section1&
         .state('form', {
-            url: '/form?application_type=i130&section1=hello1&section2=hello2&section3&section4&section5',
+            url: '/form',
             templateUrl: 'forms/form.html',
-            controller: 'formController',
-             resolve: {
-                obj2: function($stateParams){
-                    return $stateParams;
-                }
-            }
+            controller: 'formController'
         })
         
         // nested states 
@@ -49,8 +44,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
             templateProvider: function($http, $stateParams){
 
                 var obj = $stateParams;
-                var templateName = "cases/" + obj.application_type + "/" + "section2.html" ;
-                console.log("template Name: " + obj);
+                // var templateName = "cases/" + obj.application_type + "/" + "section2.html" ;
+                var templateName = 'cases/i130/section1.html'; 
+                console.log("template Name: " + templateName);
                 
 
                     return $http
@@ -94,19 +90,28 @@ app.config(function($stateProvider, $urlRouterProvider) {
         .state('form.section7', {
             url: '/section7',
             templateUrl: 'cases/i130/section7.html'
+        })
+        // url will be /form/payment
+        .state('form.section8', {
+            url: '/section8',
+            templateUrl: 'cases/i130/section8.html'
         });
-        
     // catch all route
     // send users to the form page 
-    $urlRouterProvider.otherwise('form/section1?application_type&section1&section2&section3&section4&section5');
+    $urlRouterProvider.otherwise('form/section1?application_type=i130&section1&section2&section3&section4&section5');
 });
 
 
-app.controller("formController", function($scope, obj2) {
+app.controller("formController", function($scope) {
     
     // we will store all of our form data in this object
-    $scope.formData = obj2.section1;
+    // $scope.formData = obj2.section1;
 
+
+    //function to choose forms
+    $scope.chooseForm = function() {
+        location.assign('/apps#/form/section1');
+    };
     
     // function to process the form
     $scope.processForm = function() {

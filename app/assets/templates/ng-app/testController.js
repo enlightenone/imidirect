3f88fbe0b1ff97d16d2ac82eb6e034b29c92c904
enlightenone <stevenwchow@gmail.com>
2015-04-25 20:38:53 -0700

    development of questionnaire template in progress

---
+++ app/assets/templates/ng-app/testController.js
@@ -0,0 +1,19 @@
+app.controller("frmCntrl", function($attrs, $scope,  $stateParams, $cookies, $cookieStore, myCache, Case, CaseInit, formsResource, OptionResource, $resource) {
+    $scope.formTemplate = 'templates/i130/questionnaire/i130-status.html'; 
+    $scope.frmOptions = {}; 
+
+    /* Category Templates Section */
+    $scope.formTemplate = $scope.frmOptins == ? 
+
+    /* End of Category Templates Section */
+    $scope.formFnc = function() {
+        alert($scope.frmOptions.test);
+        console.log($scope.frmOptions.test);
+
+    };
+});
+
+
+
+    
+                                    

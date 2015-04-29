e2d0f98a741697bb5a8b208abac36d50a6a2d224
enlightenone <stevenwchow@gmail.com>
2015-04-29 11:15:20 -0700

    pre i485 form creation

--- app/controllers/api/cases_controller.rb
+++ app/controllers/api/cases_controller.rb
@@ -99,12 +99,12 @@ module Api
       end
 
       # In case with I-130 multiple form application,  assign each options with false for each attribute is not defined. 
-      # if @options['i130-option'] == true 
+      if @options['i130-option'] == true 
         @case.options.create(form_id: "i130", form: "I-130", include: false) unless @case.options.find_by_form_id("i130")
         @case.options.create(form_id: "i485", form: "I-485", include: false) unless @case.options.find_by_form_id("i485")
         @case.options.create(form_id: "i131", form: "I-131", include: false) unless @case.options.find_by_form_id("i131")
         @case.options.create(form_id: "i765", form: "I-765", include: false) unless @case.options.find_by_form_id("i765")
-      # end
+      end
       render json:  {log: "Options pupulation successes"}
     end
 
@@ -269,6 +269,8 @@ private
                                                     :i765_application_renewal
                                                   )
     end
+
+    
 
     # def case_params
     #   params.require(:i130test).permit(:first_name, :last_name, :pod, :dob, :sponsor_name, :nationality, :country_of_destination, :date_of_return, :counsol, :spouse, :previous_application,  :office)

c9483a5a59eaf08a60bc6baade3fc658fb822ea7
enlightenone <stevenwchow@gmail.com>
2015-04-30 13:59:35 -0700

    populate action on cases controller is completed

--- app/controllers/api/cases_controller.rb
+++ app/controllers/api/cases_controller.rb
@@ -20,63 +20,6 @@ module Api
       @user_id = 1
       @current_case_id = params[:id]
       populate_database(@current_case_id, @user_id)
-
-      # @current_case.build_general_information(general_information_params)
-      # @current_case.build_i130(i130_params)
-      # @current_case.build_i765(i765_params) if @current_case.options.find_by(form_id: "i765").include
-      # @current_case.build_i485(i485_params) if @current_case.options.find_by(form_id: "i485").include
-
-      # if @current_case.save
-      #     @current_case_general_information = @current_case.general_information
-      #     @current_case_i485 = @current_case.i485
-      #     @current_case_i765 = @current_case.i765
-      #     @current_case_i130 = @current_case.i130
-
-
-      #     # Replace null string value with empty string in general informtion table to enable pdf form generation function
-      #     if @current_case_general_information
-      #       @current_case_general_information.attributes.each do |key, value|
-      #           if @current_case_general_information[key]== "null"
-      #              @current_case_general_information[key] = ''
-      #              @current_case_general_information.save
-      #           end
-      #       end
-      #     end
-
-      #      # Replace null string value with empty string in I765 table to enable pdf form generation function
-      #     if @current_case_i765
-      #        @current_case_i765.attributes.each do |key, value|
-      #           if @current_case_i765[key]== "null"
-      #              @current_case_i765[key] = ''
-      #              @current_case_i765.save
-      #           end
-      #       end
-      #     end
-
-      #      # Replace null string value with empty string in I-130 table to enable pdf form generation function
-      #     if @current_case_i130
-      #       @current_case_i130.attributes.each do |key, value|
-      #           if @current_case_i130[key]== "null"
-      #              @current_case_i130[key] = ''
-      #              @current_case_i130.save
-      #           end
-      #       end
-      #     end
-
-      #     if @current_case_i485
-      #       @current_case_i485.attributes.each do |key, value|
-      #           if @current_case_i485[key]== "null"
-      #              @current_case_i485[key] = ''
-      #              @current_case_i485.save
-      #           end
-      #       end
-      #     end
-
-      #   render json: {log: "Form fields population successed!"}
-      # else
-      #   render json: {log: "Form fields population failed!"}
-      # end
-
     end
 
     def option
--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -3,22 +3,33 @@ module CasesHelper
  def populate_database(current_case_id, user_id)
       @user = User.find(user_id)
       @current_case = @user.cases.find_by_case_id(current_case_id)
+      @application_id = @current_case.application_id
       @current_case.build_general_information(general_information_params)
 
       # multiple forms for I-130 application
-
       if (1..8).include? @application_id 
         @current_case.build_i130(i130_params)
         @current_case.build_i765(i765_params) if @current_case.options.find_by(form_id: "i765").include
         @current_case.build_i485(i485_params) if @current_case.options.find_by(form_id: "i485").include
       end
 
+      @current_case.build_i765(i765_params) if @application_id == 9
+      @current_case.build_i485(i485_params) if @application_id == 10
+
 
       if @current_case.save
           @current_case_general_information = @current_case.general_information
-          @current_case_i485 = @current_case.i485
-          @current_case_i765 = @current_case.i765
-          @current_case_i130 = @current_case.i130
+
+          # Multiple form I-130 application
+          if (1..8).include? @application_id 
+            @current_case_i485 = @current_case.i485
+            @current_case_i765 = @current_case.i765
+            @current_case_i130 = @current_case.i130
+          end
+
+          @current_case_i765 = @current_case.i765 if @application_id == 9 # Employment Authorization Application
+          @current_case_i485 = @current_case.i485 if @application_id == 10 # Adjustment of Status Applicatiion
+
 
           # Replace null string value with empty string in general informtion table to enable pdf form generation function
           if @current_case_general_information
@@ -26,16 +37,6 @@ module CasesHelper
                 if @current_case_general_information[key]== "null"
                    @current_case_general_information[key] = ''
                    @current_case_general_information.save
-                end
-            end
-          end
-
-           # Replace null string value with empty string in I765 table to enable pdf form generation function
-          if @current_case_i765
-             @current_case_i765.attributes.each do |key, value|
-                if @current_case_i765[key]== "null"
-                   @current_case_i765[key] = ''
-                   @current_case_i765.save
                 end
             end
           end
@@ -50,6 +51,17 @@ module CasesHelper
             end
           end
 
+           # Replace null string value with empty string in I765 table to enable pdf form generation function
+          if @current_case_i765
+             @current_case_i765.attributes.each do |key, value|
+                if @current_case_i765[key]== "null"
+                   @current_case_i765[key] = ''
+                   @current_case_i765.save
+                end
+            end
+          end
+
+
           if @current_case_i485
             @current_case_i485.attributes.each do |key, value|
                 if @current_case_i485[key]== "null"
@@ -58,6 +70,7 @@ module CasesHelper
                 end
             end
           end
+
         render json: {log: "Form fields population successed!"}
       else
         render json: {log: "Form fields population failed!"}

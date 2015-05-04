69a540b0a0ed427768555895b9194bc47f93a118
enlightenone <stevenwchow@gmail.com>
2015-05-03 20:00:40 -0700

    ng animation refactoring in progress

--- app/assets/templates/ng-app/stateConfiguration.js
+++ app/assets/templates/ng-app/stateConfiguration.js
@@ -1,7 +1,6 @@
 // configuring our routes 
 // =============================================================================
 app.config(function($stateProvider, $urlRouterProvider) {
-    
     $stateProvider
         .state('option',{
             url: '/option',
@@ -20,10 +19,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
         .state('form.section1', {
             url: '/section1',
             templateProvider: function($http, $stateParams){
-
                 var obj = $stateParams;
                 var templateName = "cases/" + obj.application_type + "/" + obj.section1 + ".html" ;
-
                     return $http
                       .get(templateName)
                       .then(function(tpl){
@@ -31,16 +28,13 @@ app.config(function($stateProvider, $urlRouterProvider) {
                       });
             },
             controller: 'formController'
-
         })
-
         // url will be /form/interests
         .state('form.section2', {
             url: '/section2',
             templateProvider: function($http, $stateParams){
                 var obj = $stateParams;
                 var templateName = "cases/" + obj.application_type + "/" + obj.section2 + ".html" ;
-                
                     return $http
                       .get(templateName)
                       .then(function(tpl){
@@ -48,18 +42,13 @@ app.config(function($stateProvider, $urlRouterProvider) {
                       });
             },
             controller: 'formController'
-
         })
-
         // url will be /form/interests
         .state('form.section3', {
             url: '/section3',
             templateProvider: function($http, $stateParams){
-
                 var obj = $stateParams;
                 var templateName = "cases/" + obj.application_type + "/" + obj.section3 +".html" ;
-                
-
                     return $http
                       .get(templateName)
                       .then(function(tpl){
@@ -68,14 +57,12 @@ app.config(function($stateProvider, $urlRouterProvider) {
             },
             controller: 'formController'
         })
-
         // url will be /form/interests
         .state('form.section4', {
             url: '/section4',
             templateProvider: function($http, $stateParams){
                 var obj = $stateParams;
                 var templateName = "cases/" + obj.application_type + "/" + obj.section4 +".html" ;
-                
                     return $http
                       .get(templateName)
                       .then(function(tpl){
@@ -83,16 +70,13 @@ app.config(function($stateProvider, $urlRouterProvider) {
                       });
             },
             controller: 'formController'
-
         })
-
         // url will be /form/interests
         .state('form.section5', {
             url: '/section5',
             templateProvider: function($http, $stateParams){
                 var obj = $stateParams;
                 var templateName = "cases/" + obj.application_type + "/" + obj.section5 +".html" ;
-
                     return $http
                       .get(templateName)
                       .then(function(tpl){
@@ -100,17 +84,13 @@ app.config(function($stateProvider, $urlRouterProvider) {
                       });
             },
             controller: 'formController'
-
         })
-
         // url will be /form/interests
         .state('form.section6', {
             url: '/section6',
             templateProvider: function($http, $stateParams){
-
                 var obj = $stateParams;
                 var templateName = "cases/" + obj.application_type + "/" + obj.section6 +".html" ;
-                
                     return $http
                       .get(templateName)
                       .then(function(tpl){
@@ -118,17 +98,13 @@ app.config(function($stateProvider, $urlRouterProvider) {
                       });
             },
             controller: 'formController'
-
         })
-        
         // url will be /form/payment
         .state('form.section7', {
             url: '/section7',
             templateProvider: function($http, $stateParams){
-
                 var obj = $stateParams;
                 var templateName = "cases/" + obj.application_type + "/" + obj.section7 +".html" ;
-
                     return $http
                       .get(templateName)
                       .then(function(tpl){
@@ -136,16 +112,13 @@ app.config(function($stateProvider, $urlRouterProvider) {
                       });
             },
             controller: 'formController'
-
         })
         // url will be /form/payment
         .state('form.section8', {
             url: '/section8',
             templateProvider: function($http, $stateParams){
-
                 var obj = $stateParams;
                 var templateName = "cases/" + obj.application_type + "/" + obj.section8 +".html" ;
-
                     return $http
                       .get(templateName)
                       .then(function(tpl){
@@ -157,10 +130,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
         .state('form.section9', {
             url: '/section9',
             templateProvider: function($http, $stateParams){
-
                 var obj = $stateParams;
                 var templateName = "cases/" + obj.application_type + "/" + obj.section9 +".html" ;
-
                     return $http
                       .get(templateName)
                       .then(function(tpl){
@@ -169,15 +140,12 @@ app.config(function($stateProvider, $urlRouterProvider) {
             },
             controller: 'formController'
         })
-        
         //Payment route
         .state('charge', {
             url: '/charge?case_id&fees_calculation_flag',
             templateUrl: 'charges/new.html',
             controller: 'formController'
         });
-
-
     // catch all route
     // send users to the form page 
     $urlRouterProvider.otherwise('form/section1?case_id&application_type=i130&section1&section2&section3&section4&section5&section6&section7&section8&section9');

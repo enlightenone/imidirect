02e20486ad19248112ea756e554a430947339eab
enlightenone <stevenwchow@gmail.com>
2015-06-02 20:37:18 -0700

    save

--- app/controllers/api/charges_controller.rb
+++ app/controllers/api/charges_controller.rb
@@ -5,19 +5,16 @@ module Api
   class ChargesController < ApplicationController
     def show
       @case_id = params[:id]
-      cookies.permanent[:caseid]= @case_id
+      # cookies.permanent[:caseid]= @case_id
       render json: fees_summary_display(@case_id) #pull fees information from database
   end
 
   def create
     # retrieve current case id from cookie
-    # @current_case_id = request.cookies["current_case_id"]
-    @current_case_id = cookies[:caseid]
-    render @current_case_id.inspect
-    # @current_case_id = session[:current_case_id]
+    @current_case_id = request.cookies["current_case_id"]
     
     # take out extra double quote from current case id string
-    # @current_case_id = @current_case_id.tr("\"","")
+    @current_case_id = @current_case_id.tr("\"","")
     fees_transaciton(@current_case_id)
 
     @current_case = Case.find_by_case_id(@current_case_id)

dabf2d04298cb926ffdff80a3a5db8c4ad8eb23d
enlightenone <stevenwchow@gmail.com>
2015-06-06 19:19:06 -0700

    case status data fetch is functionning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+  # before_action :logged_in_user, only: [:edit, :update]
+  # before_action :correct_user, only: [:edit, :update]
 
   def index
   end
@@ -11,8 +11,28 @@ class UsersController < ApplicationController
   end
 
   def show
-    
-    
+    @user = User.find(params[:id])
+    @first_name = @user.firstname
+    @last_name = @user.lastname
+
+    @cases = @user.cases.first(10)
+    @fetched_statuses = Hash.new
+    i = 1
+
+  
+    @cases.each do |c|
+      @status_questionnaire = c.status.questionnaire
+      @status_filling = c.status.filling
+      @status_payment = c.status.payment 
+
+      @fetched_statuses[i.to_s] = { "case_id" => c.case_id, 
+           "status_questionnaire" => c.status.questionnaire, "status_filling" => c.status.filling,
+           "status_payment" => c.status.payment, "status_complete" => c.status.complete}
+      i += 1
+      end
+
+    # render :json @fetched_statuses
+
   end
 
   def create

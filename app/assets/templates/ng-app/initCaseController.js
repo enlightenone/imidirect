5fcb4d5741d0182a3c72f746cc5a9c2f02d524ad
enlightenone <stevenwchow@gmail.com>
2015-06-08 11:11:28 -0700

    mid coding saving

--- app/assets/templates/ng-app/initCaseController.js
+++ app/assets/templates/ng-app/initCaseController.js
@@ -1,25 +1,57 @@
 app.controller("initCaseController", function($scope, $http, ActiveCaseStatusResource, generateCase) {
       //This method is to obtain most current user id.
-
-   $scope.caseStarts = function(app_id){
-      $http.get('/current_user').success(function(data){
-        $scope.current_user_id = data['current_user'].id ;
-        console.log("Current User Id in InitCaseController:" + $scope.current_user_id);
+   $scope.getCurrentUserId = function(current_user_id){
+     $scope.current_user_id = current_user_id;
         ActiveCaseStatusResource.get({id: $scope.current_user_id }, function(data){ 
           $scope.case_active_status = data["active_case_status"];
-
-          if($scope.current_user_id && ($scope.case_active_status == false)){
-            generateCase.initiate($scope.current_user_id, app_id)
-            location.assign( '/users/' + $scope.current_user_id + '/apps/1#/main/option?case_id=' + generateCase.case_id() + '&app_id=' + app_id);
-          }else {
-            console.log("Active Case Exist")
-          }
+           console.log("Case Active Status in InitCaseController: " + $scope.case_active_status);
+          // if($scope.current_user_id && ($scope.case_active_status == false)){
+          //   generateCase.initiate($scope.current_user_id, app_id)
+          //   location.assign( '/users/' + $scope.current_user_id + '/apps/1#/main/option?case_id=' + generateCase.case_id() + '&app_id=' + app_id);
+          // }else {
+          //   console.log("Active Case Exist")
+          // }
         });
-      }).error(function(){
-        location.assign('/sessions/new');// if not logged in, redirect to the login page
-      });
+   };
 
-      }
+   // ActiveCaseStatusResource.get({id: $scope.current_user_id }, function(data){ 
+   //        $scope.case_active_status = data["active_case_status"];
+
+   //        // if($scope.current_user_id && ($scope.case_active_status == false)){
+   //        //   generateCase.initiate($scope.current_user_id, app_id)
+   //        //   location.assign( '/users/' + $scope.current_user_id + '/apps/1#/main/option?case_id=' + generateCase.case_id() + '&app_id=' + app_id);
+   //        // }else {
+   //        //   console.log("Active Case Exist")
+   //        // }
+   //      });
+  
+
+
+
+   $scope.caseStarts = function(app_id){
+      alert($scope.case_active_status);
+       // if(!$scope.current_user_id){
+       //    location.assign('/sessions/new');
+       // }
+
+      // $http.get('/current_user').success(function(data){
+      //   $scope.current_user_id = data['current_user'].id ;
+      //   console.log("Current User Id in InitCaseController:" + $scope.current_user_id);
+      //   ActiveCaseStatusResource.get({id: $scope.current_user_id }, function(data){ 
+      //     $scope.case_active_status = data["active_case_status"];
+
+      //     if($scope.current_user_id && ($scope.case_active_status == false)){
+      //       generateCase.initiate($scope.current_user_id, app_id)
+      //       location.assign( '/users/' + $scope.current_user_id + '/apps/1#/main/option?case_id=' + generateCase.case_id() + '&app_id=' + app_id);
+      //     }else {
+      //       console.log("Active Case Exist")
+      //     }
+      //   });
+      // }).error(function(){
+      //   location.assign('/sessions/new');// if not logged in, redirect to the login page
+      // });
+
+      };//end of CaseStarts function 
 
 
 }); // end of initCaseController Controller
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @home_page_flag = true
+    @home_page_flag = true # This flag is to trigger main image display.
+    @current_user_id = (current_user)? current_user.id : "inactive"
   end
 end

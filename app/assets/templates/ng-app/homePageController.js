c03ab1e46942919558ea927fec5ba898f7bc44d0
enlightenone <stevenwchow@gmail.com>
2016-01-20 20:05:21 -0800

    Fixed issue with Firefox questionnaire link problem

--- app/assets/templates/ng-app/homePageController.js
+++ app/assets/templates/ng-app/homePageController.js
@@ -1,6 +1,6 @@
 /* The main purpose of this controller is to provide functions to generate the application case id 
    and to initiate the immigration application. */
-app.controller("1homePageController", function($scope, ActiveCaseStatusResource, generateCase) {
+app.controller("homePageController", function($scope, ActiveCaseStatusResource, generateCase) {
    /* This method is initiated at loading of the home index page. It will return user id if the active
       user is logged in and determine if the active user has on going active case. */
    $scope.getCurrentUserId = function(current_user_id){
@@ -25,20 +25,26 @@ app.controller("1homePageController", function($scope, ActiveCaseStatusResource,
      the active state of the user and case activity. If the user is not logged in, it will redirect
      to log in page. The case id is generated if there is no active ongoing case under the user  */
    $scope.applyMyCase = function(app_id){
-     alert("hello hello");
-       // if(!$scope.current_user_id){
-       //    location.assign('/sessions/new');
-       // } else if($scope.current_user_id && ($scope.case_active_status == false)) {
-       //    alert("hello");
-       //    generateCase.initiate($scope.current_user_id, app_id);
+       if(!$scope.current_user_id){
+          location.assign('/sessions/new');
+       } else if($scope.current_user_id && ($scope.case_active_status == false)) {
+          generateCase.initiate($scope.current_user_id, app_id);
         
-       //    var form_url =  '/users/' + $scope.current_user_id + '/apps/1#/main/option?case_id=' + generateCase.case_id() + '&app_id=' + app_id;
-       //    alert(form_url);
-       //    console.log(form_url);
-       //    location.assign( '/users/' + $scope.current_user_id + '/apps/1#/main/option?case_id=' + generateCase.case_id() + '&app_id=' + app_id);
-       // } else {
-       //    console.log("Active Case Already Exist");
-       // }
+          var form_url =  '/users/' + $scope.current_user_id + '/apps/1#/main/option?case_id=' + generateCase.case_id() + '&app_id=' + app_id;
+          var firefox_detection = navigator.userAgent.indexOf("Firefox");
+          var url_host = location.host;
+          var url_protocol = location.protocol;
+
+          if (firefox_detection != -1){
+            form_url = url_protocol + "//"+ url_host + form_url ;
+          } // ENnd of if statement.
+
+          location.assign(form_url);
+
+          //location.assign( '/users/' + $scope.current_user_id + '/apps/1#/main/option?case_id=' + generateCase.case_id() + '&app_id=' + app_id);
+       } else {
+          console.log("Active Case Already Exist");
+       }
   };//end of applyMyCase function 
 }); // end of initCaseController Controller
     

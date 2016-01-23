5cdb4f17cc562af3f136a9351e05f6eb37eea086
enlightenone <stevenwchow@gmail.com>
2016-01-22 19:27:38 -0800

    Commence to refactor questionnaire directive

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,17 +9,11 @@ class HomeController < ApplicationController
   end
 
   def category
-    @app_id =  params[:app_id]
-
-    if ['9', '10'].include?(@app_id) 
-      @title = "Individual Petition"
-    elsif @app_id == "1"
-      @title = "Family Petitions"
-    end 
-    #This logic is to determine the tab title on browser based on category of the application
+    @app_id =  params[:app_id] #application id from params is assigned.
 
     @home_page_flag = true # This flag is to trigger main image display.
     # Assign user id when log in or assign default value to prevent rails from breaking
     @current_user_id = (current_user)? current_user.id : "inactive" 
+
   end
 end

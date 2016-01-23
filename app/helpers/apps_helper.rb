06dcf9ba88848f90934f0a564a785da05bc67711
enlightenone <stevenwchow@gmail.com>
2016-01-23 13:17:19 -0800

    Added active application id helper

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -2,6 +2,11 @@ module AppsHelper
 
 # This method is to verify if the user has correct and active case id to access to the case.
 
+  def active_app_id 
+    @active_case = current_user.cases.find_by_active(true)
+    @active_case.application_id unless @active_case.nil?
+  end 
+
   def active_case_id
     @active_case = current_user.cases.find_by_active(true)
     @active_case.case_id unless @active_case.nil?
